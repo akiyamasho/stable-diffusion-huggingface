285e81fea943a820e89fb59b070ff7216c1619cb
秋山翔 <35907066+akiyamasho@users.noreply.github.com>
2022-08-26 08:27:34 +0900

    MAINT: use output folder and add prompt suffixes

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import logging
 import torch
@@ -5,27 +6,31 @@ import torch
 from diffusers import StableDiffusionPipeline
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+EXPORT_FOLDER = "output"
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 def generate_single(prompt: str):
-    pipe = StableDiffusionPipeline.from_pretrained(
+    pipe = StableDiffusionPipeline.from_pretrained(  # type: ignore
         "CompVis/stable-diffusion-v1-4",
         use_auth_token=True,
     ).to(DEVICE)
 
     if torch.cuda.is_available():
-        with torch.autocast(DEVICE):
+        with torch.autocast(DEVICE):  # type: ignore
             image = pipe(prompt)["sample"][0]
     else:
         image = pipe(prompt)["sample"][0]
 
-    export_filename = f"{prompt}.png"
-    image.save(export_filename)
+    # Export
+    if not os.path.exists(EXPORT_FOLDER):
+        os.makedirs(EXPORT_FOLDER)
+    export_path = f"{prompt}.png"
+    image.save(export_path)
 
-    logger.info(f"Saved to {export_filename}")
+    logger.info(f"Saved to {export_path}")
 
 
 if __name__ == "__main__":
---
+++ prompt_suffixes.md
@@ -0,0 +1,11 @@
+### Places
+
+Add your description of a place at the start of the prompt.
+
+"<description of place>, tone mapped, shiny, intricate, cinematic lighting, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by terry moore and greg rutkowski and alphonse mucha"
+
+### Humans
+
+Add your description of a human at the start of the prompt.
+
+1. "<description of human> Clean Cel shaded vector art. Shutterstock. behance hd by lois van baarle, artgerm, Helen huang, by makoto shinkai and ilya kuvshinov, rossdraws, illustration"
